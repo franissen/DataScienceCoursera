7ef3d643e4986592c6dddece75e2d149ab8bed6b
Franissen <franissen@outlook.com>
2014-05-23 00:00:52 +0200

    C2 PA2 Final

--- C2/PA2/cachematrix.R
+++ C2/PA2/cachematrix.R
@@ -1,15 +1,58 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+## With his function you can:
+##   - make a cache matrix
+##   - get/set the value of a matrix
+##   - get/set the value of the inverse of the matrix
 makeCacheMatrix <- function(x = matrix()) {
-
+    
+	inverse <- NULL
+	
+	# getting the existing matrix
+    get_matrix <- function() x
+    
+    # setting a new matrix
+    set_matrix <- function(y) {
+        x <<- y
+        inverse <<- NULL
+    }
+	
+	# getting the inverse matrix
+    get_inverse <- function() inverse
+            
+    # setting the inverse matrix
+    set_inverse <- function(inv_input) inverse <<- inv_input
+        
+    # return a list of all the functions
+    list(set_matrix = set_matrix, get_matrix = get_matrix,
+         set_inverse = set_inverse,
+         get_inverse = get_inverse)
 }
 
-
-## Write a short comment describing this function
-
+## Get the matrix generated with makeCacheMatrix, there are two options:
+##   - Inverse it has already calculated: get the value from the cache.
+##   - If not in cache: calculate the inverse and puts it in the cache (with set_inverse function)
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    #get the existing value from the cache
+    inverse <- x$get_inverse()	
+    if(is.null(inverse)) { #the cache hasn't an inverse matrix value
+		#recover the matrix
+		data <- x$get_matrix()
+		#calculate the inverse of the matrix
+		inverse <- solve(data, ...)
+		#catching the inverse for next operations
+		x$set_inverse(inverse)	 
+    }
+	return(inverse)
+    
 }
+#Example of use:
+x <- matrix(c(3,4,1,2), ncol = 2, byrow = TRUE)
+message("\nMy matrix:\n")
+print(x)
+cache <- makeCacheMatrix(x)
+message("\nInverse calculation (first time not in cache):\n")
+print(cacheSolve(cache))
+message("\nInverse calculation (result from cache):\n")
+print(cacheSolve(cache))
+message("\nVerifying the result: the matrix multiplied by the inverse it's must be the identity's matrix\n")
+print(round(x %*% y))
+
