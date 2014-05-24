762fd943c5097537c38a8579dc93d3ce8074dde0
Franissen <franissen@outlook.com>
2014-05-24 15:22:31 +0200

    Peer Assessment (Final)

---
+++ C3/PA/README.md
@@ -0,0 +1,8 @@
+Getting and Cleaning Data Course Project
+========================================
+This file describes how run_analysis.R script works.
+* Get the file in https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip and unzip it in a folder named "UCI HAR Dataset"
+* Type: source("run_analysis.R")
+* When analysis it's over there will be two output files in the same directory of run_analysis.R
+  - means_data.txt
+  - merged_data.txt
---
+++ C3/PA/run_analysis.R
@@ -0,0 +1,54 @@
+# To run this program it's required that the folder with data have the name of "UCI HAR Dataset"
+# 0) Get into memory all the required data
+activities <- read.table("UCI HAR Dataset/activity_labels.txt")
+features <- read.table("UCI HAR Dataset/features.txt")
+subject1 <- read.table("UCI HAR Dataset/test/subject_test.txt")
+subject2 <- read.table("UCI HAR Dataset/train/subject_train.txt")
+x1 <- read.table("UCI HAR Dataset/test/X_test.txt")
+x2 <- read.table("UCI HAR Dataset/train/X_train.txt")
+y1 <- read.table("UCI HAR Dataset/test/y_test.txt")
+y2 <- read.table("UCI HAR Dataset/train/y_train.txt")
+
+# 1) Merges the training and the test sets to create one data set.
+subjects <- rbind(subject1, subject2)
+x <- rbind(x1, x2)
+y <- rbind(y1, y2)
+
+# 2) Extracts only the measurements on the mean and standard deviation for each measurement. 
+indices_of_good_features <- grep("-mean\\(\\)|-std\\(\\)", features[, 2])
+x <- x[, indices_of_good_features]
+names(x) <- features[indices_of_good_features, 2]
+names(x) <- tolower(gsub("\\(|\\)", "", names(x)))
+#names(x) <- tolower(names(x))
+
+# 3) Uses descriptive activity names to name the activities in the data set
+activities[, 2] = gsub("_", "", tolower(as.character(activities[, 2])))
+y[,1] = activities[y[,1], 2]
+names(y) <- "activity"
+
+# 4) Appropriately labels the data set with descriptive activity names. 
+names(subjects) <- "subject"
+merged <- cbind(subjects, y, x)
+
+write.table(merged, "merged_data.txt")
+
+# 5) Creates a second, independent tidy data set with the average of each variable for each activity and each subject. 
+num_activities = length(activities[,1])
+num_subjects = length(unique(subjects)[,1])
+unique_subjects = unique(subjects)[,1]
+
+num_cols = dim(merged)[2]
+result = merged[1:(num_activities*num_subjects), ]
+
+row = 1
+for (s in 1:num_subjects) {
+  for (a in 1:num_activities) {
+    result[row, 1] = unique_subjects[s]
+    result[row, 2] = activities[a, 2]
+    temp <- merged[merged$subjects==s & merged$activity==activities[a, 2], ]
+    result[row, 3:num_cols] <- colMeans(temp[, 3:num_cols])
+    row = row + 1
+  }
+}
+
+write.table(result, "means_data.txt")
